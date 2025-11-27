a458e665c6ed7467f052faf29392ee6db46ba33a
Kolja Sam <github@koljapluemer.com>
2025-11-27 17:51:14 +0100

    hide top bar

--- src/main/main.ts
+++ src/main/main.ts
@@ -74,6 +74,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
